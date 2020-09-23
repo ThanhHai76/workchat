18531ea08f362c1c9e21db9a49dc2b3ab3dc9c0c
ThanhHai76 <haint62@wru.vn>
2020-09-23 10:12:14 +0700

    merge test-merge to profile

--- chatwork/src/app/components/home/home.component.ts
+++ chatwork/src/app/components/home/home.component.ts
@@ -8,12 +8,8 @@ import { Subject } from 'rxjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-<<<<<<< HEAD
   profilemerge: string;
-=======
-
   testmerge: string;
->>>>>>> test-merge
   destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor() { }
